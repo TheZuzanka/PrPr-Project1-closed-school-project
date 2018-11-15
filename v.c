2353ecde0ac5fbf4403ff6466247cdc6b8a0ffb7
Your Name <you@example.com>
2018-11-15 17:29:17 +0100

    if statements if NULL chenged

--- v.c
+++ v.c
@@ -6,16 +6,18 @@
 void v(PERSON **p_list, int n_notes) {
     int i;
 
-    if (*p_list != NULL) {                                                                                              //ak bola vykonaná funkcia o alebo a teda list nie je prázdny, vypíše sa
-        for (i = 0; i < n_notes; i++) {
-            printf("meno priezvisko: %s\n"
-                           "pohlavie: %c\n"
-                           "rok narodenia: %s\n"
-                           "SPZ: %s\n"
-                           "typ priestupku: %d\n"
-                           "vyska pokuty: %d\n"
-                           "datum priestupku: %s\n\n", (*p_list)[i].name, (*p_list)[i].sex, (*p_list)[i].birth_year,
-                   (*p_list)[i].SPZ, (*p_list)[i].type, (*p_list)[i].money, (*p_list)[i].date);
-        }
+    if (*p_list == NULL) {                                                                                              //ak bola vykonaná funkcia o alebo a teda list nie je prázdny, vypíše sa
+        return;
+    }
+    
+    for (i = 0; i < n_notes; i++) {
+        printf("meno priezvisko: %s\n"
+                       "pohlavie: %c\n"
+                       "rok narodenia: %s\n"
+                       "SPZ: %s\n"
+                       "typ priestupku: %d\n"
+                       "vyska pokuty: %d\n"
+                       "datum priestupku: %s\n\n", (*p_list)[i].name, (*p_list)[i].sex, (*p_list)[i].birth_year,
+               (*p_list)[i].SPZ, (*p_list)[i].type, (*p_list)[i].money, (*p_list)[i].date);
     }
 }
